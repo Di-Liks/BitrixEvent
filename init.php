b44b1ed7cd4fe6b90c256938892a8d3e8d72302c
Dill <60818081+Di-Liks@users.noreply.github.com>
2024-10-18 16:22:52 +0300

    Update init.php

--- init.php
+++ init.php
@@ -10,32 +10,36 @@
         if ($arFields['IBLOCK_ID'] == $iblockModelsId) {
             
             // Получаем текущее значение элемента
-            $element = CIBlockElement::GetByID($arFields['ID'])->GetNext();
-            
-            // Проверяем, установлено ли поле "архивировать" в значение "Архивировать"
-            if ($arFields['PROPERTY_VALUES']['ARCHIVE_AUTO'][0]['VALUE_XML_ID'] == '1') {
-                // Если да, то создаем элемент в инфоблоке "Архив моделей авто"
-                $el = new CIBlockElement;
-                
-                // Параметры нового элемента
-                $arLoadArray = array(
-                    "IBLOCK_ID"      => $iblockArchiveId,
-                    "NAME"           => $element['NAME'], // Имя модели
-                    "PROPERTY_VALUES"=> array(
-                        "MARK_AUTO"  => $element['PROPERTY_MARKA_AUTO_VALUE'], // Марка
-                        "MODEL_AUTO" => $element['PROPERTY_MODEL_AUTO_VALUE'], // Модель
-                        "PHOTO_AUTO" => $element['PROPERTY_PHOTO_AUTO_VALUE'], // Фото
-                    ),
-                    "ACTIVE"         => "Y", // Активность элемента
-                );
-                
-                // Добавляем элемент в архивный инфоблок
-                if ($newElementId = $el->Add($arLoadArray)) {
-                    // Элемент успешно добавлен в архив
-                    AddMessage2Log("Элемент добавлен в архив с ID: ".$newElementId);
-                } else {
-                    // Ошибка при добавлении элемента
-                    AddMessage2Log("Ошибка добавления элемента в архив: ".$el->LAST_ERROR);
+            $element = CIBlockElement::GetByID($arFields['ID'])->GetNextElement();
+            if ($element) {
+                $arProps = $element->GetProperties(); // Получаем свойства элемента
+                $elementFields = $element->GetFields(); // Получаем поля элемента
+
+                // Проверяем, установлено ли поле "архивировать" в значение "Архивировать"
+                if ($arFields['PROPERTY_VALUES']['ARCHIVE_AUTO'][0]['VALUE_XML_ID'] == '1') {
+                    // Если да, то создаем элемент в инфоблоке "Архив моделей авто"
+                    $el = new CIBlockElement;
+                    
+                    // Параметры нового элемента
+                    $arLoadArray = array(
+                        "IBLOCK_ID"      => $iblockArchiveId,
+                        "NAME"           => $elementFields['NAME'], // Имя модели
+                        "PROPERTY_VALUES"=> array(
+                            "MARK_AUTO"  => $arProps['MARK_AUTO']['VALUE'], // Марка
+                            "MODEL_AUTO" => $arProps['MODEL_AUTO']['VALUE'], // Модель
+                            "PHOTO_AUTO" => $arProps['PHOTO_AUTO']['VALUE'], // Фото
+                        ),
+                        "ACTIVE"         => "Y", // Активность элемента
+                    );
+                    
+                    // Добавляем элемент в архивный инфоблок
+                    if ($newElementId = $el->Add($arLoadArray)) {
+                        // Элемент успешно добавлен в архив
+                        AddMessage2Log("Элемент добавлен в архив с ID: ".$newElementId);
+                    } else {
+                        // Ошибка при добавлении элемента
+                        AddMessage2Log("Ошибка добавления элемента в архив: ".$el->LAST_ERROR);
+                    }
                 }
             }
         }
